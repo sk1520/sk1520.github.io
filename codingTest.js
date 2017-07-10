703457a9f009695c534178e67150f0dbdc36abb6
sk1520 <stanislavkim16@hotmail.com>
2017-07-10 16:32:35 -0400

    2

--- codingTest.js
+++ codingTest.js
@@ -11,8 +11,7 @@ function imageChange(){
    
 
     if(image_tracker == 0){
-       $('#interval-image').hide();
-      $('#interval-image').fadeIn(500);
+       
        $('#interval-image').attr('src', 'https://scontent-lga3-1.cdninstagram.com/t51.2885-15/e35/19436250_100501617256984_4799286182712180736_n.jpg')
 
    image_tracker ++;
@@ -20,8 +19,7 @@ function imageChange(){
      
     
    else if(image_tracker == 1){
-    $('#interval-image').hide();
-      $('#interval-image').fadeIn(500);
+  
   $('#interval-image').attr('src', 'https://scontent-lga3-1.cdninstagram.com/t51.2885-15/e35/19367615_1892480557681308_1451688882939625472_n.jpg'); 
     image_tracker ++;
      
@@ -29,8 +27,7 @@ function imageChange(){
   }
   else{
        
-  $('#interval-image').hide();
-      $('#interval-image').fadeIn(500);
+  
       $('#interval-image').attr('src', 'https://scontent-lga3-1.cdninstagram.com/t51.2885-15/e35/19120672_138833393342644_1357844242659016704_n.jpg'); 
    
     image_tracker =0;
